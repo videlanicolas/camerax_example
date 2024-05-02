b3f33664c203373420046c1af5ef852f3e1743fb
Nicolas Videla <videlanicolas@gmail.com>
2024-05-02 16:18:20 +1000

    Adding stuff.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,6 +53,7 @@ android {
 }
 
 dependencies {
+    implementation(libs.accompanist.permissions)
     implementation(libs.androidx.camera.core)
     implementation(libs.androidx.camera.camera2)
     implementation(libs.androidx.camera.lifecycle)
--- app/src/main/java/com/android/example/cameraxexample/MainActivity.kt
+++ app/src/main/java/com/android/example/cameraxexample/MainActivity.kt
@@ -19,6 +19,8 @@ import androidx.camera.video.Recording
 import androidx.camera.video.VideoCapture
 import androidx.camera.view.LifecycleCameraController
 import androidx.camera.view.PreviewView
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -48,13 +50,44 @@ class MainActivity : ComponentActivity() {
         setContent {
             CameraXExampleTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    CameraScreen(
-                        modifier = Modifier.padding(innerPadding)
-                    )
+
+                    FeatureThatRequiresCameraPermission()
                 }
             }
         }
 
+    }
+}
+
+@OptIn(ExperimentalPermissionsApi::class)
+@Composable
+private fun FeatureThatRequiresCameraPermission() {
+
+    // Camera permission state
+    val cameraPermissionState = rememberPermissionState(
+        android.Manifest.permission.CAMERA
+    )
+
+    if (cameraPermissionState.status.isGranted) {
+        CameraScreen()
+    } else {
+        Column {
+            val textToShow = if (cameraPermissionState.status.shouldShowRationale) {
+                // If the user has denied the permission but the rationale can be shown,
+                // then gently explain why the app requires this permission
+                "The camera is important for this app. Please grant the permission."
+            } else {
+                // If it's the first time the user lands on this feature, or the user
+                // doesn't want to be asked again for this permission, explain that the
+                // permission is required
+                "Camera permission required for this feature to be available. " +
+                        "Please grant the permission"
+            }
+            Text(textToShow)
+            Button(onClick = { cameraPermissionState.launchPermissionRequest() }) {
+                Text("Request permission")
+            }
+        }
     }
 }
 
